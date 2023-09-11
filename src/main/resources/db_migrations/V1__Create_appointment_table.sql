7c5315e20f716a016a47f83409f77a01a950968b
sechmo <sesanchezmo@unal.edu.co>
2023-09-11 11:42:30 -0500

    Added appointment status value

--- src/main/resources/db_migrations/V1__Create_appointment_table.sql
+++ src/main/resources/db_migrations/V1__Create_appointment_table.sql
@@ -1,8 +1,13 @@
+CREATE TYPE appointment_status_enum AS enum('Pending','Canceled','Attended','Missed');
+CREATE TYPE notification_status_enum AS enum('ToNotify','Notified','ToCancel');
+
 CREATE TABLE appointment (
     id SERIAL NOT NULL PRIMARY KEY,
     date TIMESTAMP NOT NULL,
     doctor_id INT NOT NULL,
     patient_id INT NOT NULL,
     medical_record_id INT NOT NULL,
-    date_of_scheduling TIMESTAMP  NOT NULL DEFAULT NOW()
+    date_of_scheduling TIMESTAMP  NOT NULL DEFAULT NOW(),
+    status appointment_status_enum NOT NULL,
+    notification_status notification_status_enum NOT NULL
 );
--- src/main/scala/com/medisync/quickstart/domain/Appointments.scala
+++ src/main/scala/com/medisync/quickstart/domain/Appointments.scala
@@ -6,13 +6,32 @@ import monix.newtypes.integrations.DerivedCirceCodec
 import doobie.util.meta.Meta
 import NewtypesDoobie._
 
-import doobie._
-import doobie.implicits._
-import doobie.implicits.javasql._
 import cats.Show
 import NewtypesHttp4s._
 
-object Appointments {
+import scala.util.Try
+
+import doobie._
+import doobie.implicits._
+import doobie.implicits.javasql._
+import doobie.postgres._
+import doobie.postgres.implicits._
+import org.http4s.CacheDirective.public
+import com.medisync.quickstart.Doctors.DoctorId
+import io.circe._
+import io.circe.syntax._
+
+object Appointments:
+  
+  type TestId = TestId.Type
+  object TestId
+      extends NewtypeWrapped[Int]
+      with DerivedCirceCodec
+      with DerivedDoobieCodec
+      with DerivedHttp4sParamCodec
+  
+
+
   type AppointmentId = AppointmentId.Type
   object AppointmentId
       extends NewtypeWrapped[Int]
@@ -34,13 +53,82 @@ object Appointments {
       with DerivedDoobieCodec
       with DerivedHttp4sParamCodec
 
+  enum AppointmentStatus extends Enum[AppointmentStatus]:
+    case Pending, Canceled, Attended, Missed
+
+  given Meta[AppointmentStatus] =
+    pgJavaEnum[AppointmentStatus]("appointment_status_enum")
+
+  enum NotificationStatus extends Enum[NotificationStatus]:
+    case ToNotify, Notified, ToCancel, Canceled
+
+  given Meta[NotificationStatus] =
+    pgJavaEnum[NotificationStatus]("notification_status_enum")
+
   case class Appointment(
       id: AppointmentId,
       date: Instant,
-      doctorId: Int,
+      doctorId: DoctorId,
       patientId: PatientId,
       medicalRecordId: MedicalRecordId,
-      dateOfScheduling: Instant
+      dateOfScheduling: Instant,
+      status: AppointmentStatus,
+      notificationStaus: NotificationStatus
   )
 
-}
+  object  Appointment:
+    given encodeInstant: Encoder[Instant] = Encoder.encodeString.contramap[Instant](_.toString)
+
+    given decodeInstant: Decoder[Instant] = Decoder.decodeString.emapTry { str =>
+      Try(Instant.parse(str))
+    }
+
+    given Encoder[AppointmentStatus] = Encoder.encodeString.contramap[AppointmentStatus](_.toString())
+    given Encoder[NotificationStatus] = Encoder.encodeString.contramap[NotificationStatus](_.toString())
+    given Encoder[Appointment] = new Encoder[Appointment]:
+      final def apply(app: Appointment): Json = Json.obj(
+        ("id",app.id.asJson),
+        ("date",app.date.asJson),
+        ("doctor_id",app.doctorId.asJson),
+        ("patiend_id",app.patientId.asJson),
+        ("medical_record_id",app.medicalRecordId.asJson),
+        ("date_of_scheduling",app.dateOfScheduling.asJson),
+        ("status", app.status.asJson),
+        ("notification_status",app.notificationStaus.asJson)
+      ) 
+
+
+    given Read[Appointment] =
+      Read[
+        (
+            AppointmentId,
+            Instant,
+            DoctorId,
+            PatientId,
+            MedicalRecordId,
+            Instant,
+            AppointmentStatus,
+            NotificationStatus
+        )
+      ].map {
+        case (
+              appId,
+              date,
+              docId,
+              patId,
+              medRecId,
+              dateOfSch,
+              status,
+              notifStatus
+            ) =>
+          Appointment(
+            appId,
+            date,
+            docId,
+            patId,
+            medRecId,
+            dateOfSch,
+            status,
+            notifStatus
+          )
+    }
